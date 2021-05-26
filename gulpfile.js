7c8a3ebe35580e1c604bda8fc517df238c8c21ea
caigk <caigk@sina.com>
2021-05-26 16:50:14 +0800

    加入培训录像

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 var rename = require("gulp-rename");
 
+const through2 = require('through2');
+var Path = require('path');
+
 function optize(cb) {
   //压缩js
   src('docs/kchis/**/*.js')
@@ -27,4 +30,28 @@ function optize(cb) {
 }
 
 exports.build = optize;
-//exports.default = series(test1,111);
+
+function t1(cb) {
+  src('docs/pdf/*.pdf')
+    .pipe(through2.obj(function (originalFile, _, cb) {
+      var file = originalFile.clone({ contents: false });
+      const fp = file.relative;
+
+      //处理路径
+      // console.log(Path.parse(fp));
+      // console.log(Path.basename(fp, '.pdf'));
+      console.log( originalFile.relative+' => '+ Path.join(Path.dirname(fp), Path.basename(fp, '.pdf') + '.html'));
+      file.path = Path.join(file.base, Path.join(Path.dirname(fp), Path.basename(fp, '.pdf') + '.html'));
+
+      //生成内容
+      file.contents = Buffer.from('ddd1');
+
+      //回调
+      cb(null, file);
+    }))
+    .pipe(dest('pdfT/'));
+
+  cb();
+}
+
+exports.t1 = t1;
